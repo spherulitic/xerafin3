4c0bd2c9ec270a97a7ca11a20b5a6b1a56cbce08
Chris Lipe <spherulitic@pop-os.localdomain>
2022-04-09 13:20:10 -0500

    Committer: Chris Lipe <chris@xerafin.net>
     Code cleanup
    
     On branch main
     Changes to be committed:
    	new file:   .gitignore
    	new file:   .pylintrc
    	modified:   cardbox/cardbox/__init__.py

--- cardbox/cardbox/__init__.py
+++ cardbox/cardbox/__init__.py
@@ -1,9 +1,9 @@
+'''Xerafin service containing cardbox functionality'''
+
 from flask import Flask
+from cardbox import views
 
 app = Flask(__name__)
 
-from cardbox import views
-
 if __name__ == "__main__":
   app.run(debug=True)
-
