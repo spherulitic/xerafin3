9926ac096ee9119e50cfaa32a5eb94856fe3ba2f
Chris Lipe <chris@xerafin.net>
2022-12-10 19:28:33 -0600

    Replace getRankings.php
    Prepared the stats service for keycloak integration, mysql env variables, etc.
    Note mysql connectivity has not been tested. This will be tested with the subtasks to this (#66-#69)
    Edited widget.rankings.js to call the endpoint instead of the php. This is the only place getRankings.php is referenced.
    Moved the four class .php files to the stats subdir. In subtasks #66-#69 these will be replaced by .py class definitions which will then be imported into views.py and instantiated by the /getRankings endpoint to generate the proper response dict/JSON

--- stats/stats/views.py
+++ stats/stats/views.py
@@ -1,8 +1,13 @@
 from stats import app
-from flask import request, jsonify, Response
+from flask import request, jsonify, Response, g, session
+import json, urllib, requests, jwt
 from logging.config import dictConfig
 import sys
 import xerafinUtil.xerafinUtil as xs
+#from .rankings import Rankings
+#from .slothRankings import SlothRankings
+#from .awards import Awards
+#from .metaranks import Metaranks
 
 dictConfig({
     'version': 1,
@@ -20,9 +25,73 @@ dictConfig({
     }
 })
 
+@app.before_request
+def get_user():
+  public_key_url = 'http://keycloak:8080/auth/realms/Xerafin'
+  with urllib.request.urlopen(public_key_url) as r:
+    public_key = json.loads(r.read())['public_key']
+    public_key = f'''-----BEGIN PUBLIC KEY-----
+{public_key}
+-----END PUBLIC KEY-----'''
+  raw_token = request.headers["Authorization"]
+  g.auth_token = jwt.decode(raw_token, public_key, audience="x-client", algorithms=['RS256'])
+  g.uuid = g.auth_token["sub"]
+
+  g.mysqlcon = xs.getMysqlCon()
+  g.con = g.mysqlcon.cursor()
+
+  return None
+
+@app.after_request
+def close_mysql(response):
+  g.con.close()
+  g.mysqlcon.close()
+  return response
+
+
 @app.route("/", methods=['GET', 'POST'])
 def default():
   return "Xerafin Stats Service"
+
+@app.route("/getRankings", methods=['GET', 'POST'])
+def getRankings():
+  return jsonify(["Dummy response"])
+  try:
+    data = request.get_json(force=True)
+
+    # data should contain such items as:
+    #   displayType
+    #   pageSize  - int
+    #   pageNumber - int
+    #   timeframe {today, yesterday, thisWeek, lastWeek, thisMonth,
+    #              lastMonth, thisYear, lastYear, eternity}
+    #   type
+    #   year
+    #   view {QA, AW, SL}
+
+    if data.get("view", "QA") == "QA":
+      if data.get("displayType", 0) in (2, 3):
+  #      rank = new Metaranks(data)
+        pass
+      else:
+  #      rank = new Rankings(data)
+        pass
+    elif data.get("view", "QA") == "AW":
+  #    rank = new Awards(data)
+      pass
+    elif data.get("view", "QA") == "SL":
+  #    rank = new SlothRankings(data)
+      pass
+    else:
+  #    rank = new Rankings(data)
+      pass
+
+  except:
+    xs.errorLog()
+    error["status"] = "An error occurred. See log for more details"
+
+#  return jsonify(rank.get_data())
+  return jsonify(["Dummy response"])
 
 @app.route("/getAllTimeStats", methods=['GET', 'POST'])
 def getAllTimeStats():
@@ -31,17 +100,15 @@ def getAllTimeStats():
   error = { }
 
   try:
-    with xs.getMysqlCon() as con:
-      cur = con.cursor()
-      command = "select ifnull(sum(questionsAnswered), 0), ifnull(count(distinct userid), 0) from leaderboard"
-      cur.execute(command)
-      row = cur.fetchone()
-      if row:
-        result["questions"] = int(row[0])
-        result["users"] = int(row[1])
-      else:
-        result["questions"] = 0
-        result["users"] = 0
+    command = "select ifnull(sum(questionsAnswered), 0), ifnull(count(distinct userid), 0) from leaderboard"
+    g.con.execute(command)
+    row = g.con.fetchone()
+    if row:
+      result["questions"] = int(row[0])
+      result["users"] = int(row[1])
+    else:
+      result["questions"] = 0
+      result["users"] = 0
 
   except Exception as ex:
     xs.errorLog()
