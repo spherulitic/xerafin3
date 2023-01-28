c3df558a828cd47804e7b875551b11143b92b6cf
Chris Lipe <chris@xerafin.net>
2023-01-27 23:14:25 -0600

    Added end-of-day activity to daily cron
    * Added lb_summary and daily chat to endpoint in stats service
    * Added daily award calculation to dailySummary endpoint in stats
    * NEXT: generate daily quizzes
    * Fixed bug in login service now that the service account is a
       logged in user

--- login/login/views.py
+++ login/login/views.py
@@ -115,6 +115,9 @@ def getLoggedInUsers():
   sessionList = [{"userId": x["userId"], "lastAccess": x["lastAccess"]} for x in sessions]
   for row in sessionList:
     userInfo = keycloak_admin.get_user(row["userId"])
+    if userInfo['username'] == 'chris@xerafin.net':
+      # This is the service account
+      continue
     row["name"] = f'{userInfo["firstName"]} {userInfo["lastName"]}'
     row["photo"] = userInfo['attributes'].get('photo', 'images/unknown_player.gif')
     result.append(row)
