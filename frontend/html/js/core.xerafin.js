ab7c6c10262b0090ae857ae61e9214fdfa2026e4
Chris Lipe <chris@xerafin.net>
2026-09-10 20:42:52 -0500

    fix(frontend): use Word Wall Dark palette for Invaders colors (143)

--- frontend/html/js/core.xerafin.js
+++ frontend/html/js/core.xerafin.js
@@ -108,8 +108,8 @@ Config.prototype = {
       ['Cardbox Invaders','initInvaders','images/cardboxInvaders2.png']
     ];
     this.colorAnswers = [
-      'black','#dddddd','hsl(0,75%,50%)','hsl(25,75%,50%)','hsl(50,75%,50%)','hsl(75,75%,50%)','hsl(100,75%,50%)','hsl(150,75%,50%)',
-      'hsl(175,75%,50%)','hsl(200,75%,50%)','hsl(225,75%,50%)','hsl(250,75%,50%)','hsl(275,75%,50%)','hsl(300,75%,50%)'
+      "black", "azure", "plum", "palegreen", "yellowgreen", "khaki", "salmon",
+      "hotpink", "crimson", "magenta", "chocolate", "peachpuff", "mistyrose", "floralwhite"
     ];
     this.colorAnswersOld = [
       "black", "azure", "mediumorchid", "palegreen", "yellowgreen", "khaki", "salmon", "hotpink",
